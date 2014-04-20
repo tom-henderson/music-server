fcabd4ca8f0d17b6d805a8433da0c3b23cb83632
Tom Henderson <tomhenderson@mac.com>
2014-04-20 17:54:25 +1200

    Remove dropbox urls

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# https://www.dropbox.com/s/on6170d1904g8ww/setup.sh
 # Music Server Setup
 
 # Update system
@@ -10,12 +9,3 @@ apt-get -y install alsa alsa-tools alsa-utils alsa-oss unzip
 
 # Set up music user
 sudo usermod -aG audio music
-
-#mkdir /home/music/log
-
-# Download Sample Music
-#wget https://www.dropbox.com/s/lomwe0sfbv15yqk/Music.zip
-#unzip Music.zip -d /home/music/music/
-#rm Music.zip
-
-
