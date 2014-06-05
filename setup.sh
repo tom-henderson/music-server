31b0bff43843acf966fb410fbd28c845788adbaf
Tom Henderson <tomhenderson@mac.com>
2014-06-06 09:34:51 +1200

    Reorganise project files

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@
 # Update system
 sudo apt-get update && sudo apt-get -y upgrade
 
-# Install packages from apt
-apt-get -y install alsa alsa-tools alsa-utils alsa-oss unzip 
+# Install apps
+#sudo ./install/install-logitechmediaserver
+sudo ./install/install-squeezeslave
+sudo ./install/install-alsacap
 
 # Set up music user
 sudo usermod -aG audio music
